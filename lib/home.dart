c9e3fd622682e3d7486bd0fa45fbe4c8175e9321
unknown <nancykumari620@gmail.com>
2023-07-17 16:37:22 +0530

    Contact_list completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:contact_list/contact.dart';
 import 'package:flutter/material.dart';
 
 class Myhome extends StatefulWidget {
@@ -8,70 +9,81 @@ class Myhome extends StatefulWidget {
 }
 
 class _MyhomeState extends State<Myhome> {
-  @override
+  TextEditingController nameController = TextEditingController();
+  TextEditingController contactController = TextEditingController();
+  List<Contact> contacts = List.empty(growable: true);
+  int selectedIndex = -1;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 183, 17, 225),
+        backgroundColor: const Color.fromARGB(255, 183, 17, 225),
         centerTitle: true,
-        title: Text("Contacts List"),
+        title: const Text("Contacts List"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Padding(padding: EdgeInsets.all(10)),
-            Container(
-              padding: EdgeInsets.all(10),
-              width: 400.0,
-              height: 70.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 2.0,
+            const Padding(padding: EdgeInsets.all(10)),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "Contact Name",
+                hintStyle: const TextStyle(fontSize: 20),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: "Contact Number", border: InputBorder.none),
-              ),
             ),
-            Padding(padding: EdgeInsets.all(10)),
-            Container(
-              padding: EdgeInsets.all(10),
-              width: 400.0,
-              height: 70.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 2.0,
+            const Padding(padding: EdgeInsets.all(10)),
+
+            TextField(
+              controller: contactController,
+              keyboardType: TextInputType.number,
+              maxLength: 10,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: "Contact Name", border: InputBorder.none),
+                hintText: "Contact Number",
+                hintStyle: const TextStyle(fontSize: 20),
               ),
             ),
-            Padding(padding: EdgeInsets.all(10)),
+            // ),
+
+            const Padding(padding: EdgeInsets.all(5)),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(padding: EdgeInsets.all(40)),
                 Container(
                   width: 70,
                   height: 40,
                   decoration:
                       BoxDecoration(borderRadius: BorderRadius.circular(30)),
                   child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("save"),
+                    onPressed: () {
+                      String name = nameController.text.trim();
+                      String contact = contactController.text.trim();
+                      if (name.isNotEmpty && contact.isNotEmpty) {
+                        setState(() {
+                          nameController.text = '';
+                          contactController.text = '';
+                          contacts.add(Contact(name: name, contact: contact));
+                        });
+                      }
+                    },
+                    child: const Text(
+                      "save",
+                      style: TextStyle(fontSize: 16),
+                    ),
                     style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(255, 183, 17, 225),
+                      primary: const Color.fromARGB(255, 183, 17, 225),
                     ),
                   ),
                 ),
-                Padding(padding: EdgeInsets.all(40)),
+                const Padding(padding: EdgeInsets.all(30)),
                 Container(
                   width: 90,
                   height: 40,
@@ -79,16 +91,97 @@ class _MyhomeState extends State<Myhome> {
                     borderRadius: BorderRadius.circular(30),
                   ),
                   child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Update"),
+                    onPressed: () {
+                      String name = nameController.text.trim();
+                      String contact = contactController.text.trim();
+                      if (name.isNotEmpty && contact.isNotEmpty) {
+                        setState(() {
+                          nameController.text = '';
+                          contactController.text = '';
+                          contacts[selectedIndex].name = name;
+                          contacts[selectedIndex].contact = contact;
+                        });
+                        selectedIndex = -1;
+                      }
+                    },
+                    child: const Text(
+                      "Update",
+                      style: TextStyle(fontSize: 16),
+                    ),
                     style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(255, 183, 17, 225),
+                      primary: const Color.fromARGB(255, 183, 17, 225),
                     ),
                   ),
                 )
               ],
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            contacts.isEmpty
+                ? const Text(
+                    "No contact yet",
+                    style: TextStyle(fontSize: 22, color: Colors.purple),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: contacts.length,
+                      itemBuilder: (context, index) => getRow(index),
+                    ),
+                  )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget getRow(int index) {
+    return Card(
+      child: ListTile(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(contacts[index].name),
+            Text(contacts[index].contact),
+          ],
+        ),
+        leading: CircleAvatar(
+          backgroundColor:
+              index % 2 == 0 ? Colors.deepPurple : Colors.purpleAccent,
+          child: Text(
+            contacts[index].name[0],
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        trailing: SizedBox(
+          width: 70,
+          child: Row(
+            children: [
+              InkWell(
+                child: Icon(Icons.edit),
+                onTap: () {
+                  setState(() {
+                    nameController.text = contacts[index].name;
+                    contactController.text = contacts[index].contact;
+                    selectedIndex = index;
+                  });
+                },
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    contacts.removeAt(index);
+                  });
+                },
+                child: Icon(Icons.delete),
+              )
+            ],
+          ),
         ),
       ),
     );
